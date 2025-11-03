5e65b6d1fe61adb5c0086b4fd0cb9c177e72d775
VanJackal <v@njackal.com>
2025-11-03 11:46:00 -0600

    updated fabric api version

--- src/client/java/com/njackal/BuildingUtilsClient.java
+++ src/client/java/com/njackal/BuildingUtilsClient.java
@@ -1,10 +1,12 @@
 package com.njackal;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents;
 
 public class BuildingUtilsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BuildingUtils.LOGGER.info("Client Initialized");
 	}
 }
